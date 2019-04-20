4dc7326d91e897734acc81250e0f853305200c2d
horlumiday <oluwadareysamuel@yahoo.com>
2019-04-20 15:58:54 +0100

    store api default link to a variable

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
 
-	//store page number
-	var pageNumber = 1;
+			//store page number
+			var pageNumber = 1;
 
-	//listen to a click event
-	$('#submit').click(function () {
+			//listen to a click event
+			$('#submit').click(function () {
+				
+				//get element from DOM
+				var searchInput = $('#search');
+
+				//default api link
+				var url = ' https://api.unsplash.com/';
+
 
 		
-		}
 
 
-	});
-
-
-
+			});
