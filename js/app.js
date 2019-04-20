db011b3657ee5aa77dc483118d0d1fcbc255b09c
horlumiday <oluwadareysamuel@yahoo.com>
2019-04-20 15:59:59 +0100

    build up link

--- js/app.js
+++ js/app.js
@@ -5,15 +5,24 @@ $(document).ready(function () {
 
 			//listen to a click event
 			$('#submit').click(function () {
-				
+
 				//get element from DOM
 				var searchInput = $('#search');
 
 				//default api link
 				var url = ' https://api.unsplash.com/';
 
+				//when input#search is not empty
+				if (searchInput.val() !== '') {
 
-		
+					var toLowerCase = searchInput.val().toLowerCase();
+
+					url += `search/photos?page=${pageNumber}&query=${toLowerCase}&per_page=12`;
+
+					//add key to api link
+					url += `&client_id=95b50323e9088ff9cb2368e19fc9f970a5c08b945fbc3fbc55972e1180989fbc`;
+
+				}
 
 
 			});
